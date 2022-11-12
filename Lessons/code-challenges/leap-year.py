03d2ed759bd9262ec76a420d81f22ea58f5cb912
Jonjo Jefferson <jonjo.jefferson@gmail.com>
2022-11-12 00:53:20 +0000

    updated leap year

--- Lessons/code-challenges/leap-year.py
+++ Lessons/code-challenges/leap-year.py
@@ -1,21 +1,12 @@
 year = int(input("Which year do you want to check? "))
 
-divide_one_hundred = year / 100
-divide_four_hundred = year / 400
-
-# DEBUG
-
-# print(divide_four)
-# print(divide_one_hundred)
-# print(divide_four_hundred)
-
-if (year % 4) != 0:
-    print("Not leap year")
-elif (year % 2) == 0:
-    if (divide_one_hundred % 2) != 0:
-        print("Leap year")
-    else:
-        if (divide_four_hundred % 2) == 0:
+if (year % 4) == 0:
+    if (year % 100) == 0:
+        if (year % 400) == 0:
             print("Leap year")
         else:
-            print("Not leap year")
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
