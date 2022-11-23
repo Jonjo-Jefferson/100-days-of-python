406908c513563fbe7fed8abc548f635afeeff493
Jonjo Jefferson <jonjo.jefferson@gmail.com>
2022-11-23 20:31:32 +0000

    started days in month

---
+++ Lessons/functions-with-outputs.py
@@ -0,0 +1,12 @@
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didnt provide valid inputs"
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return formatted_f_name + " " + formatted_l_name
+
+
+formated_string = format_name(
+    input("What is your first name?"), input("What is your last name?")
+)
+print(formated_string)
--- Lessons/testing.py
+++ Lessons/testing.py
@@ -22,8 +22,15 @@ import random
 # cheapest_pizza = sorted[0]
 # priciest_pizza = sorted[-1]
 
-random_words = ["apple", "pepperoni", "potato", "sausage"]
-i = random.randint(0, len(random_words) - 1)
-print(f"The random number is {i} - " + random_words[i])
-for word in random_words:
-    print(word)
+# random_words = ["apple", "pepperoni", "potato", "sausage"]
+# i = random.randint(0, len(random_words) - 1)
+# print(f"The random number is {i} - " + random_words[i])
+# for word in random_words:
+#     print(word)
+
+
+chance_to_rain = [85, 80, 79, 94, 88]
+print(sum(chance_to_rain))
+for chance in chance_to_rain:
+    average_rain = sum(chance_to_rain) / len(chance_to_rain)
+print(round(average_rain))
